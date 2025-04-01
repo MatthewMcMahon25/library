7473708e29b065367a02a9c4bc07dab2fa92b217
MatthewMcMahon25 <macmatt5@gmail.com>
2025-04-01 16:38:18 -0400

    Create the display for any books in the library
    The program displays any books that are currently
    in the library

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// # 2.
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(cover, title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
+  this.cover = cover;
   this.title = title;
   this.author = author;
   this.pages = pages
@@ -12,26 +12,66 @@ function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary(cover, title, author, pages, read) {
+  myLibrary.push(new Book(cover, title, author, pages, read));
 }
 
-addBookToLibrary("The 4-Hour Workweek", "Tim Ferriss", 448, true);
-addBookToLibrary("Never Split the Difference", "Chris Voss", 288, true);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 304, false);
-addBookToLibrary("Captivate", "Vanessa Van Edwards", 320, true);
+addBookToLibrary("images/4HourWorkWeek.jpg", "The 4-Hour Workweek", "Tim Ferriss", 448, true);
+addBookToLibrary("images/neverSplitTheDifference.jpg", "Never Split the Difference", "Chris Voss", 288, true);
+addBookToLibrary("images/theHobbit.jpg", "The Hobbit", "J.R.R. Tolkien", 304, false);
+addBookToLibrary("images/captivate.jpg", "Captivate", "Vanessa Van Edwards", 320, true);
 
-// #3
 function display() {
+    const container = document.querySelector(".bookContainer");
+
     for (let item in myLibrary) {
-        console.log(myLibrary[item]);
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("card");
+
         for (let prop in myLibrary[item]) {
-            console.log(myLibrary[item][prop]);
+            const value = myLibrary[item][prop];
+
+            if (prop === 'cover') {
+                const img = document.createElement("img");
+                img.src = value;
+                img.alt = "A cover of the book";
+                img.classList.add("cover-image");
+
+                bookDiv.appendChild(img);
+            }
+
+            else if (prop === 'title') {
+                const h3 = document.createElement("h3");
+                h3.textContent = value;
+                
+                bookDiv.appendChild(h3);
+            }
+
+            else if (prop === 'author' || prop === 'pages') {
+                const p = document.createElement("p");
+                p.textContent = prop.charAt(0).toUpperCase() + prop.slice(1)
+                                 + ': ' + value;
+
+                bookDiv.appendChild(p);
+            }
+
+            else if (prop === 'read') {
+                if (value === true) {
+                    const p = document.createElement("p");
+                    p.textContent = "Read" + ": " + '✅';
+
+                    bookDiv.appendChild(p);
+                } else {
+                    const p = document.createElement("p");
+                    p.textContent = "Read" + ": " + '❌';
+
+                    bookDiv.appendChild(p);
+                }
+            }
+
+            container.appendChild(bookDiv);
         }
     }
 }
 
 display();
-
-
-// 1. Write a function that loops through arrary
